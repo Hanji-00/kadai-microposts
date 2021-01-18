40ce787dc2a16307ca6338d5e84a5bdb31f40fbb
tomohiro.hanji <hanji.tomohiro@gmail.com>
2021-01-18 14:47:38 +0000

    first

---
+++ app/models/micropost.rb
@@ -0,0 +1,9 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  
+  has_many :favorites, dependent: :destroy
+  has_many :fav_users, through: :favorites, source: :user
+  
+  
+  validates :content, presence: true, length: {maximum: 255}
+end
